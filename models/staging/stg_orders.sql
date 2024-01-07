24d5f11cf3e5bad44c1c94aa947d5af0e8b04642
s10242881@connect.np.edu.sg <s10242881@connect.np.edu.sg>
2024-01-07 07:25:09 +0000

    added markup.sql to new branch add-markup-to-macros-folder, inserted 	{{ markup() }} as markup in stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,6 +1,3 @@
-{{ config (materialized='view')}}
-
-
 select
 --from raw_orders
 orderid,
@@ -19,6 +16,7 @@ country,
 category,
 productname,
 subcategory,
+{{ markup() }} as markup,
 ordersellingprice - ordercostprice as orderprofit
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
